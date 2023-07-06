5afb6ffd5b7b0961c2331ad5a1607d4c1de38771
Waris123 <warisanjumrox@gmail.com>
2023-07-07 00:26:34 +0500

    Add files via upload

--- Assignment#02/main.ts
+++ Assignment#02/main.ts
@@ -1,1 +1,7 @@
+/* Personal Message: Store a person’s name in a variable, 
+and print a message to that person. Your message should be simple, such as, 
+“Hello Eric, would you like to learn some Python today?”
+*/
 
+let person_name = 'Muhammad Waris Anjum';
+console.log('Salam '+person_name+', would you like to learn some TypeScript today?');
